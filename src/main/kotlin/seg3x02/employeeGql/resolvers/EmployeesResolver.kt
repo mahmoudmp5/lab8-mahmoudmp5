4f3302233e4451dfaf9d937a43fbd32c2867cef2
mahmoudmp5 <mahmoudmp5q@gmail.com>
2024-11-13 22:33:17 -0500

    Update EmployeesResolver.kt

--- src/main/kotlin/seg3x02/employeeGql/resolvers/EmployeesResolver.kt
+++ src/main/kotlin/seg3x02/employeeGql/resolvers/EmployeesResolver.kt
@@ -1,8 +1,31 @@
 package seg3x02.employeeGql.resolvers
 
-import org.springframework.stereotype.Controller
+import graphql.kickstart.tools.GraphQLMutationResolver
+import graphql.kickstart.tools.GraphQLQueryResolver
+import org.springframework.stereotype.Component
+import seg3x02.employeeGql.entity.Employee
+import seg3x02.employeeGql.repository.EmployeesRepository
+import seg3x02.employeeGql.resolvers.types.CreateEmployeeInput
 
-@Controller
-class EmployeesResolver {
 
+@Component
+class EmployeesResolver(
+    private val employeesRepository: EmployeesRepository
+) : GraphQLQueryResolver, GraphQLMutationResolver {
+
+    fun employees(): List<Employee> = employeesRepository.findAll()
+
+    fun employee(id: Long): Employee? = employeesRepository.findById(id.toString()).orElse(null)
+
+    fun addEmployee(input: CreateEmployeeInput): Employee {
+        val employee = Employee(
+            name = input.name,
+            dateOfBirth = input.dateOfBirth,
+            gender = input.gender,
+            city = input.city,
+            salary = input.salary,
+            email = input.email
+        )
+        return employeesRepository.save(employee)
+    }
 }

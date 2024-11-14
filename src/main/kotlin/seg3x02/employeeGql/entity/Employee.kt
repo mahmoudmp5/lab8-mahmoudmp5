96197d126a24955f4958155f60fe3943606c76b6
mahmoudmp5 <mahmoudmp5q@gmail.com>
2024-11-13 22:32:34 -0500

    Update Employee.kt

--- src/main/kotlin/seg3x02/employeeGql/entity/Employee.kt
+++ src/main/kotlin/seg3x02/employeeGql/entity/Employee.kt
@@ -5,10 +5,10 @@ import org.springframework.data.mongodb.core.mapping.Document
 
 @Document(collection = "employee")
 data class Employee(
-        val name: String,
-        val dateOfBirth: String,
-        val city: String,
-        val salary: Float,
+        val name: String?,
+        val dateOfBirth: String?,
+        val city: String?,
+        val salary: Float?,
         val gender: String?,
         val email: String?
 ) {
